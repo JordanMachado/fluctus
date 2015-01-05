70794302fe1401746d424b47e9bffd68ce699a6c
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-01-05 23:59:40 +0100

    Add mrdoob/stats.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,24 @@
-var webgl, gui;
+var webgl, gui, stats, container;
 
 window.onload = init;
 
 function init(){
-    webgl = new Webgl(window.innerWidth, window.innerHeight);
-    document.querySelector('.three').appendChild(webgl.renderer.domElement);
+    container = document.querySelector('.three');
 
+    webgl = new Webgl(window.innerWidth, window.innerHeight);
+    container.appendChild(webgl.renderer.domElement);
+
+    // dat gui
     gui = new dat.GUI();
     gui.close();
+
+    // stats
+    stats = new Stats();
+    stats.setMode(0); // 0: fps, 1: ms
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    container.appendChild( stats.domElement );
 
     window.onresize = resizeHandler;
 
@@ -20,5 +31,6 @@ function resizeHandler() {
 
 function animate() {
     requestAnimationFrame(animate);
+    stats.update();
     webgl.render();
 }
